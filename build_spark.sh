559224ce8d2cfbb42f45525a585741340bf089cc
Franky Romero <faromero@stanford.edu>
2022-01-24 19:54:36 +0000

    Update script to build spark
    Includes commands for building with Kubernetes

--- build_spark.sh
+++ build_spark.sh
@@ -8,3 +8,15 @@ pushd python
 python3 setup.py sdist
 popd
 
+###
+# Build with Kubernetes support
+# ./build/mvn -Pkubernetes -DskipTests clean package
+
+# Build PySpark container
+# repo_name="faromero"
+# sudo ./bin/docker-image-tool.sh -r ${repo_name} -t latest -p ./resource-managers/kubernetes/docker/src/main/dockerfiles/spark/bindings/python/Dockerfile build
+
+# Push containers
+# You likely need to do "sudo docker login" first
+# sudo ./bin/docker-image-tool.sh -r ${repo_name} -t latest push
+
